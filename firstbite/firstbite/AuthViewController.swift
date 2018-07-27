4a7ade4654075f07288fc6eb0c54aa4e5f6991ab
Leon Trieu <leontrieu@leons-mac.local>
2018-07-27 03:24:52 -0700

    pushing winston & master merge

--- firstbite/firstbite/AuthViewController.swift
+++ firstbite/firstbite/AuthViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class AuthViewController: UIViewController {
 
--- firstbite/firstbite/Profile.swift
+++ firstbite/firstbite/Profile.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseFirestore
+import FirebaseAuth
 
 class Profile: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
