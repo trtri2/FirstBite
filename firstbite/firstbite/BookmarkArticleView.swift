2bc125062c179e531f30525df7246923f304c591
Leon Trieu <leontrieu@leons-mac.local>
2018-07-26 20:53:07 -0700

    initial commit

---
+++ firstbite/firstbite/BookmarkArticleView.swift
@@ -0,0 +1,24 @@
+//
+//  BookmarkArticleView.swift
+//  BoringSSL
+//
+//  Created by Leon Trieu on 2018-07-26.
+//
+
+import Foundation
+import UIKit
+import FirebaseFirestore
+
+class BookmarkArticleView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
