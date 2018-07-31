878aaed374ffd8add8203877875973748e3b4bf9
thtrieu <thtrieu@d142-058-163-247.wireless.sfu.ca>
2018-07-30 22:01:22 -0700

    added BMITable.swift, coding the calculator.

--- firstbite/firstbite/BMITableViewController.swift
+++ firstbite/firstbite/BMITableViewController.swift
@@ -15,7 +15,51 @@ import FirebaseFirestore
 
 class BMITableViewController: UIViewController{
     
- var fstore = Firestore.firestore()
+    var fstore = Firestore.firestore()
+    var userID = Auth.auth().currentUser!.uid
+    
+    //text field var for height and weight
+    @IBOutlet weak var textHeight: UITextField!
+    @IBOutlet weak var textWeight: UITextField!
+    
+    //text field var for result
+    @IBOutlet weak var textResult: UITextField!
+    
+    var height = Int()
+    var weight = Int()
+
+    
+    //when pressed, collects the user's height and weight from profile in database
+    @IBAction func collectProfile(_ sender: Any) {
+     var tempArr = Array<Int>()
+        fstore.collection(userID).document("Profile").getDocument(completion:{(snapshot,error) in
+            if let doc = snapshot?.data(){
+           
+            tempArr = doc["Child_heights"] as! Array<Int>
+            self.textHeight.text = String(tempArr[tempArr.count-1])
+                
+            tempArr = doc["Child_weights"] as! Array<Int>
+            self.textWeight.text = String(tempArr[tempArr.count-1])
+            
+            
+            }
+      //      calculate()
+        })
+        
+    }
+    
+    //calculates BMI using kg/m^2
+    func calculate(finalHeight: Int, finalWeight: Int){
+        var result = Int()
+        
+        textResult.text = String(result)
+    }
+    
+    @IBAction func pressCalc(_ sender: Any) {
+    }
+    
+    
+
 override func viewDidLoad(){
     super.viewDidLoad()
     
