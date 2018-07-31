90e0fa73048ed6eb3d46a1a7997074f6e52ae447
Han Yang <hanyang@hans-mac.local>
2018-07-31 00:01:33 -0700

    more progress on reminder

--- firstbite/firstbite/ReminderViewController.swift
+++ firstbite/firstbite/ReminderViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ReminderViewController: UIViewController, UITableViewDataSource {
+class ReminderViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var table: UITableView!
     var data:[String] = []
@@ -18,16 +18,60 @@ class ReminderViewController: UIViewController, UITableViewDataSource {
 
         // Do any additional setup after loading the view.
         table.dataSource = self
+        table.delegate = self
+        self.title = "Reminder"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addReminder))
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationItem.leftBarButtonItem = editButtonItem
+        load()
     }
 
+    @objc func addReminder() {
+        if table.isEditing {
+            return
+        }
+        let name:String = "Item \(data.count + 1)"
+        data.insert(name, at: 0)
+        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
+        table.insertRows(at: [indexPath], with: .automatic)
+        save()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = UITableViewCell()
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier:"cell")!
         cell.textLabel?.text = data[indexPath.row]
         return cell
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        table.setEditing(editing, animated: animated)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        data.remove(at: indexPath.row)
+        table.deleteRows(at: [indexPath], with: .fade)
+        save()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(data[indexPath.row])")
+    }
+    
+    func save() {
+        UserDefaults.standard.set(data, forKey: "reminder")
+    }
+    
+    func load() {
+        if let loadedData:[String] = UserDefaults.standard.value(forKey: "reminder") as? [String] {
+            data = loadedData
+            table.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
